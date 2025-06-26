02f71ede6416d970b9aa2f81412105cffb3465af
Gary Allen <gvcallen@gmail.com>
2025-06-26 17:24:36 +0200

    Trying to fix hash error

--- pmrf/models/lumped.py
+++ pmrf/models/lumped.py
@@ -123,8 +123,8 @@ class Capacitor(Model):
 
         z0_0 = z0_1 = self.z0
         denom = 1.0 + 1j * w * C * (z0_0 + z0_1)
-        s11 = (1.0 - 1j * w * C * (z0_0.conj() - z0_1) ) / denom
-        s22 = (1.0 - 1j * w * C * (z0_1.conj() - z0_0) ) / denom
+        s11 = (1.0 - 1j * w * C * (np.conj(z0_0) - z0_1) ) / denom
+        s22 = (1.0 - 1j * w * C * (np.conj(z0_1) - z0_0) ) / denom
         s12 = s21 = (2j * w * C * (z0_0.real * z0_1.real)**0.5) / denom
 
         s = np.array([
