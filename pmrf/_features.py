Fixed model reload bug and added extra derivatives

pmrf/_features.py
@@ -82,6 +82,9 @@ def make_feature_function(
     freq: Frequency | skrf.Frequency,
     dtype: jnp.dtype = jnp.complex128,
     flat = False,
+    return_params = False,
+    return_recon_fn = False,
+    nderiv = 0,
     jit = False,
 ) -> tuple[FeatureFunctionT, ModelParametersT] | tuple[FeatureFunctionT, ModelParametersT, Callable]:
     """Generate a feature function to parametrically extract model features.
@@ -101,8 +104,11 @@ def make_feature_function(
         freq (pmrf.Frequency):                      The frequency to extract the features at, treated as a static argument.
         dtype (jnp.dtype, optional):                The data type of the final out feature matrix.        
         flat (bool):                                Whether the feature function should accept a flat array as input.
-                                                    If True, a third argument will be returned, which is a "reconstruct" function
-                                                    that transforms the flat array back into the full (unraveled-combined) model.
+        return_params (bool):                       Specifies that the parameters should be returned alongside the feature function.
+                                                    For the flat case, the parameters are returned as an array. Defaults to `False`.
+        return_recon_fn (bool):                     Specifies that a "reconstruct" function should also be returned to
+                                                    reconstruct the model from its arguments.
+        nderiv (int):                               The number of derives to take of the feature function. Default to 0 for the original function.
         jit (bool):                                 Whether or not to just-in-time compile the function.
 
     Returns:
@@ -129,17 +135,28 @@ def make_feature_function(
             return extract_features(model_recon, features, freq, dtype=dtype)
     else:
         params_out = params_tree
+        def reconstruct_fn(params_tree) -> Model:
+            return combine(params_tree, static)        
+        
         def feature_fn(tree_params) -> jnp.ndarray:
             model_recon = combine(tree_params, static)
             return extract_features(model_recon, features, freq, dtype=dtype)
+
+    for i in range(nderiv):
+        feature_fn = jax.jacfwd(feature_fn)
+            
     
     if jit:
         feature_fn = jax.jit(feature_fn)
+        if return_recon_fn:
+            reconstruct_fn = jax.jit(reconstruct_fn)
         
-    if flat:
-        return feature_fn, params_out, reconstruct_fn
-
-    return feature_fn, params_out
+    if return_params:
+        if return_recon_fn:
+            return feature_fn, params_out, reconstruct_fn
+        else:
+            return feature_fn, params_out
+    return feature_fn
 
 def _format_features(features: FeatureInputT) -> list[FeatureT]:
     if isinstance(features, dict):

pmrf/_frequency.py
@@ -51,8 +51,7 @@ class Frequency(eqx.Module):
     _f: jnp.array
     _unit: str = field(static=True)
 
-    def __init__(self, start: float = 0, stop: float = 0, npoints: int = 0, f = None,
-        unit: FrequencyUnitT | None = 'Hz') -> None:
+    def __init__(self, start: float = 0, stop: float = 0, npoints: int = 0, unit: FrequencyUnitT | None = 'Hz', f = None) -> None:
         """
         Frequency initializer.
 

pmrf/fitting/fitters/_polychord.py
@@ -7,7 +7,6 @@ import numpy as np
 
 from pmrf._features import make_feature_function
 from pmrf.fitting._bayesian import BayesianFitter, BayesianResults
-from pmrf._features import make_feature_function
 from pmrf._util import time_string
 
 def norm_logpdf(x, loc=0.0, scale=1.0):
@@ -68,7 +67,7 @@ class PolychordFitter(BayesianFitter):
         
         # Generate prior and likelihood functions
         self.logger.info("Compiling model and likelihood function...")
-        feature_fn, x0, recon_fn = make_feature_function(self.initial_model, self.feature_list, self.model_frequency, flat=True)        
+        feature_fn, x0, recon_fn = make_feature_function(self.initial_model, self.feature_list, self.model_frequency, flat=True, return_params=True, return_recon_fn=True)
         x0_with_likelihood = list(x0) + [self.likelihood_params['sigma'].prior.mean]
         def jax_likelihood(flat_params_with_sigma) -> jnp.ndarray:
             sigma = flat_params_with_sigma[-1]

pmrf/sampling/_base.py
@@ -26,7 +26,6 @@ class BaseSampler(ABC):
 
         N = self.N
         params_matrix = self._generate_param_matrix(N)
-        models = []
         params, static = self.model.params()
         _, ravel_fn = flatten_util.ravel_pytree(params)
         for i in N:
@@ -79,7 +78,7 @@ class BaseSampler(ABC):
             frequency = Frequency.from_skrf(frequency)
         
         params_matrix = jnp.array(self._generate_param_matrix(N))
-        feature_fn, params_out, _reconstruct_fn = make_feature_function(self.model, features, frequency, dtype=dtype, flat=True, jit=not dont_jit)
+        feature_fn, params_out = make_feature_function(self.model, features, frequency, dtype=dtype, jit=not dont_jit, flat=True, return_params=True)
         self.logger.info('Compiling feature function...')
         _features0 = feature_fn(params_out)
 