49d4b680bbedb4221a5654dc83b72fdc2a478d92
Gary Allen <gvcallen@gmail.com>
2026-01-28 13:49:31 +0200

    Updated TrainableDistribution docs

--- pmrf/distributions/trainable.py
+++ pmrf/distributions/trainable.py
@@ -1,39 +1,54 @@
 import logging
 from typing import TypeVar
 from abc import abstractmethod
-import jax.numpy as jnp
 
+import jax
+import jax.numpy as jnp
 from numpyro.distributions import Distribution
-import numpy as np
 
 from pmrf.distributions.sampled import SampledDistribution
 
 class TrainableDistribution(Distribution):
     @classmethod
     @abstractmethod
-    def from_samples(cls, samples: jnp.ndarray, weights: jnp.ndarray | None = None, key=None, init_kwargs=None, **train_kwargs):
-        raise NotImplementedError
-
-    @classmethod
-    def from_sampled_distribution(cls, sampled_distribution: SampledDistribution, weighted=False, key=None, init_kwargs=None, **train_kwargs) -> 'TrainableDistribution':
+    def from_samples(cls, samples: jnp.ndarray, weights: jnp.ndarray | None = None, key: jnp.ndarray | None = None, init_kwargs: dict | None = None, **train_kwargs):
         """
-        Train this distribution from a sampled distribution.
+        Train this distribution directly from samples.
 
         Parameters
         ----------
-        train_distribution : TrainableDistributionT or None, optional
-            The distribution class to train. If None, defaults to `MargarineMAFDistribution`.
+        samples : jnp.ndarray
+            The samples to use.
+        weights: jnp.ndarray, optional
+            The weights to use for training, if any. Defaults to ``None``.
+        key : jnp.ndarray
+            The random key for training.
+        init_kwargs : dict, optional
+            Initialization arguments passed to the underlying distribution for initialization.
+        **train_kwargs
+            Training arguments passed to the underlying distribution for training.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def from_sampled_distribution(cls, sampled_distribution: SampledDistribution, weighted: bool = False, key: jnp.array | None = None, init_kwargs: dict | None = None, **train_kwargs) -> 'TrainableDistribution':
+        """
+        Train this distribution on a sampled distribution.
+
+        Parameters
+        ----------
+        sampled_distribution : SampledDistribution
+            The sampled distribution to train this one on.
         weighted : bool, optional, default=False
             If False, uses weights for training; otherwise uses equal weights.
+        key : jnp.ndarray
+            The random key for training.
+        init_kwargs : dict, optional
+            Initialization arguments passed to the underlying distribution for initialization.
         **train_kwargs
-            Additional keyword arguments passed to the distribution's training method.
+            Training arguments passed to the underlying distribution for training.
         """
         training_data: jnp.ndarray = sampled_distribution.samples(weighted=weighted)[:,0:sampled_distribution.num_params]
-
-        # Formula to broaden the flow
-        # scale = np.abs(np.mean(training_data, axis=0)) * drift_sigma
-        # training_data += np.random.normal(loc=0.0, scale=scale, size=training_data.shape)
-        
         logging.info(f'Training distribution on {sampled_distribution.param_names()}')
 
         if not weighted:
@@ -42,6 +57,30 @@ class TrainableDistribution(Distribution):
             dist = cls.from_samples(training_data, weights=sampled_distribution.weights(), key=key, init_kwargs=init_kwargs, **train_kwargs)
         
         return dist
+    
+    @classmethod
+    def from_distribution(cls, distribution: Distribution, num_samples: int = 10000, key: jnp.array | None = None, init_kwargs: dict | None = None, **train_kwargs) -> 'TrainableDistribution':
+        """
+        Train this distribution on another distribution.
+
+        Parameters
+        ----------
+        distribution : Distribution
+            The distribution to train this one on.
+        num_samples: int
+            The number of samples to get for training data. Defaults to 10000.
+        key : jnp.ndarray
+            The random key for training.
+        init_kwargs : dict, optional
+            Initialization arguments passed to the underlying distribution for initialization.
+        **train_kwargs
+            Training arguments passed to the underlying distribution for training.
+        """
+        sample_key, train_key = jax.random.split(key)
+        training_data: jnp.ndarray = distribution.sample(sample_key, sample_shape=(num_samples,))
+        dist = cls.from_samples(training_data, key=train_key, init_kwargs=init_kwargs, **train_kwargs)
+        
+        return dist    
     
     
 TrainableDistributionT = TypeVar("TrainableDistributionT", bound=TrainableDistribution)
