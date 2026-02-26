8df3aeabfc86c5f2c0704fa596a0e3ba81050ac7
Gary Allen <gvcallen@gmail.com>
2026-02-26 08:55:37 +0200

    Removed surrogate field sampler

--- pmrf/sampling/_algorithms/__init__.py
+++ pmrf/sampling/_algorithms/__init__.py
@@ -1,4 +1,3 @@
 from pmrf.sampling._algorithms.uniform import *
 from pmrf.sampling._algorithms.latin_hypercube import *
 from pmrf.sampling._algorithms.field import *
-from pmrf.sampling._algorithms.surrogate_field import *
