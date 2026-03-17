f17dec02aa9550f1726ae3fe949047d9c7230a4f
Gary Allen <gvcallen@gmail.com>
2026-03-17 11:28:23 +0200

    Updated optimize_model

--- pmrf/optimize/base.py
+++ pmrf/optimize/base.py
@@ -2,16 +2,12 @@ from typing import Callable, Sequence
 
 import jax.numpy as jnp
 import equinox as eqx
-import optimistix as optx
-import numpyro.distributions.transforms as transforms
 
 from pmrf.model import Model
 from pmrf.frequency import Frequency
-from pmrf.transforms import HypercubeTransform
 from pmrf.goal import Goal
 from pmrf.optimize.problem import OptimizeProblem
 from pmrf.optimize.result import OptimizeResult
-from pmrf.optimize.backends.optimistix import OptimistixOptimizer
 
 class AbstractOptimizer(eqx.Module):
     """
@@ -28,10 +24,20 @@ def optimize_model(
     model: Model,
     frequency: Frequency,
     cost: Callable[[Model, Frequency], jnp.ndarray] | Sequence[Goal],
-    solver: AbstractOptimizer | optx.AbstractMinimiser = optx.BFGS(),
+    solver: AbstractOptimizer | 'optimistix.AbstractMinimizer' = None,
 ) -> OptimizeResult:
-    if isinstance(solver, optx.AbstractMinimiser):
-        solver = OptimistixOptimizer(solver)
+
+    if solver is None:
+        import optimistix as optx
+        solver = optx.LBFGS()
+
+    if not isinstance(solver, AbstractOptimizer):
+        import optimistix as optx
+        if isinstance(solver, optx.AbstractMinimiser):
+            from pmrf.optimize.backends.optimistix import OptimistixOptimizer
+            solver = OptimistixOptimizer(solver)
+        else:
+            raise Exception(f"Unknown solver class: {type(solver)}")
 
     problem = OptimizeProblem(model, frequency, cost)
     results = solver.solve(problem)
