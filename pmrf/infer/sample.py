5c78ca26b507d08af3e30c703e163cb457a37b8c
Gary Allen <gvcallen@gmail.com>
2026-04-07 17:12:33 +0200

    Updated is_infered docs

--- pmrf/infer/sample.py
+++ pmrf/infer/sample.py
@@ -16,7 +16,8 @@ def is_inferer(x):
     """
     Returns if a solver is suitable for Bayesian inference in :mod:`pmrf.infer`.
 
-    Returns `True` for :class:`infx.PolyChord`.
+    Returns `True` for :class:`infx.AbstractSampler`, :class:`infx.AbstractHostHypercubeNestedSampler`
+    and :class:`infx.AbstractHostPhysicalNestedSampler`.
     """    
     return isinstance(x, infx.AbstractSampler | infx.AbstractHostHypercubeNestedSampler | infx.AbstractHostPhysicalNestedSampler)
 
