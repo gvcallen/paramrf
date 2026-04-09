8b2ca53f38507fa6f00e69629d53424cc1236d17
Gary Allen <gvcallen@gmail.com>
2026-04-09 02:31:37 +0200

    Fixed fit result doc error

--- pmrf/optimize/result.py
+++ pmrf/optimize/result.py
@@ -13,16 +13,16 @@ class OptimizeResult(prx.Module):
     """
     The result of an optimization run.
     """
-    model: Model
     #: The RF model holding the final optimized parameters.
+    model: Model
 
-    objective_fn: Callable[[Model, Frequency], jnp.ndarray]
     #: The objective function (e.g., :class:`pmrf.evaluators.TargetLoss`)
     #: used to calculate the objective during optimization. If the objective was an module
     #: with hyper-parameters, then this contains the optimized objective model.
+    objective_fn: Callable[[Model, Frequency], jnp.ndarray]
 
-    value: jnp.ndarray
     #: The final objective function value achieved by the optimizer.
+    value: jnp.ndarray
     
-    solver_results: Any = None
     #: The underlying solution object returned by the solver, if any.
+    solver_results: Any = None
