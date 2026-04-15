45997dca962805291972c84d03c9acdd3f88f827
Gary Allen <gvcallen@gmail.com>
2026-04-15 15:13:03 +0200

    Added bayesian inference to fitting functions

--- pmrf/fitting/minimize.py
+++ pmrf/fitting/minimize.py
@@ -1,6 +1,3 @@
-
-
-from functools import partial
 from typing import Callable
 
 import jax.numpy as jnp
@@ -8,17 +5,19 @@ try:
     import skrf
 except ImportError:
     pass
+
 import parax as prx
 
+import distreqx.distributions as dist
+
 from pmrf.core import Model, Frequency
-from pmrf.math import CONVERSION_LOOKUP, LOSS_LOOKUP
-from pmrf.constants import Optimizer, AggregationKind
+from pmrf.constants import Optimizer
 from pmrf.network_collection import NetworkCollection
 from pmrf.models import Measured
-from pmrf.evaluators import Feature, TargetLoss
+from pmrf.evaluators import Feature, TargetLoss, MarginalLogLikelihood, NegativeLogLikelihood, NegativeLogPosterior
+from pmrf.likelihoods import GaussianLikelihood
 from pmrf.losses import RMSELoss
 
-from pmrf.optimize.result import OptimizeResult
 from pmrf.optimize.minimize import minimize
 from pmrf.optimize.solvers import ScipyMinimizer
 from pmrf.fitting.result import FitResult
@@ -30,9 +29,11 @@ def fit_minimize(
     solver: Optimizer = ScipyMinimizer(),
     *,
     features: str | list[str] | Callable = 's',
-    loss_fn: Callable = RMSELoss(),
-    multioutput: AggregationKind | None = None,
-    scale_fn: str | Callable | None = None,
+    inference: str = 'frequentist',
+    loss_fn: Callable = None,
+    likelihood: Callable = None,
+    noise: prx.Parameter | Callable[[jnp.ndarray], jnp.ndarray] = None,
+    discrepancy_model: Callable[[jnp.ndarray, jnp.ndarray], dist.AbstractDistribution] | None = None,    
     **kwargs,
 ) -> FitResult:
     """
@@ -60,18 +61,40 @@ def fit_minimize(
         Can either be function, a callable PyTree with optional parameters, or a string,
         in which case a feature evaluator is created (see :class:`pmrf.evaluators.Feature`).
         Defaults to all S-parameters.
-    loss_fn : str | Callable, default=RMSELoss()
-        The loss function between the model prediction and the data.
-        Can be a function, a callable PyTree with optional parameters, or a string
-        for a lookup into :data:`pmrf.math.LOSS_LOOKUP`
+    inference : str
+        The type of inference to use, either 'frequentist' or 'bayesian'.
+        See `loss_fn` and `likelihood` for more information.
+    loss_fn : str | Callable, optional
+        A loss function between the model prediction and the data.
+        Can be a function or a callable PyTree with optional parameters.
+        Used to internally create a :class:`pmrf.evaluators.TargetLoss` evaluator.
+        Mutually exclusive with `likelihood`. If neither `loss_fn` nor `likelihood` is passed,
+        :class:`pmrf.losses.RMSELoss` is used for `loss_fn` if `inference` is 'frequentist',
+        otherwise :class:`pmrf.likelihoods.GaussianLikelihood` is used for `likelihood`.
         See :mod:`pmrf.losses` for common losses.
-        Defaults to `None`, in which case :class:`pmrf.losses.RMSELoss` is used.
-    multioutput : Aggregation, optional
-        An additional key-word parameter to optionally pass to `loss_fn` indicating
-        how to aggregate outputs. For the default of None, the argument is not passed.
-    scale_fn : str | Callable, default=None
-        A scaling to apply to the output metric after aggregation.
-        Can be a string for a lookup into :data:`pmrf.math.CONVERSION_LOOKUP`.
+    likelihood : str | Callable, optional
+        A likelihood model representing the probability of observing the data.
+        Can be a function or a callable PyTree with optional parameters.
+        Used to internally create a :class:`pmrf.evaluators.NegativeLogLikelihood`
+        or :class:`pmrf.evaluators.NegativeLogPosterior` evaluator.
+        Mutually exclusive with `loss_fn`. If neither `loss_fn` nor `likelihood` is passed,
+        :class:`pmrf.losses.RMSELoss` is used for `loss_fn` if `inference` is 'frequentist',
+        otherwise :class:`pmrf.likelihoods.GaussianLikelihood` is used for `likelihood`.
+        See :mod:`pmrf.losses` for common losses.
+    noise : prx.Parameter | Callable[[jnp.ndarray], jnp.ndarray], optional
+        Likelihood noise, either a fixed parameter, or a callable that accepts
+        a model prediction (in event space) and returns noise parameters
+        for a Gaussian likelihood. Mutually exclusive with `likelihood_fn`.
+        For the function case, can be a callable PyTree with optional parameters.
+        See :mod:`pmrf.noise_models` for built-in noise models.
+        Defaults to `None`, in which case uniform variance from 0.0 to 0.1 is constructed internally.
+        Only used if a `likelihood` is passed or `inference` is 'bayesian'.
+    discrepancy_model : Callable[[jnp.ndarray, jnp.ndarray], jnp.ndarray | dist.AbstractDistribution], optional
+        A discrepancy model, which caters for the discrepancy between the model and measured data.
+        Can either be a function, or a callable PyTree with optional parameters.
+        To use a Gaussian process as a discrepancy model,
+        see :class:`pmrf.discrepancy_models.GaussianProcess`.
+        Only used if a `likelihood` is passed or `inference` is 'bayesian'.
     **kwargs : dict
         Additional keyword arguments passed to the underlying solver.
 
@@ -82,7 +105,9 @@ def fit_minimize(
     """
     # Error checking
     if isinstance(data, jnp.ndarray) and frequency is None:
-        raise Exception("Frequency must be passed if Network data is not provided")
+        raise ValueError("Frequency must be passed if Network data is not provided")
+    if loss_fn is not None and likelihood is not None:
+        raise ValueError("Only one of either `loss_fn` or `likelihood` can be past to `fit_minimize`")
     
     # Resolve data and features
     if not isinstance(features, Callable):
@@ -96,24 +121,27 @@ def fit_minimize(
         target = features(Measured(data), frequency)
     else:
         target = data
-    
-    # Resolve the loss model    
-    if isinstance(loss_fn, str):
-        loss_fn = LOSS_LOOKUP[loss_fn][1]
-    if multioutput is not None:
-        loss_fn = partial(loss_fn, multioutput=multioutput)
-    objective_fn = TargetLoss(loss=loss_fn, predictor=features, target=target)
-    
-    # Append an optional scale function
-    if isinstance(scale_fn, str):
-        scale_fn = CONVERSION_LOOKUP[scale_fn][1]
-    if scale_fn is not None:
-        scaled_cost_fn = prx.op.Map(scale_fn, objective_fn)
+
+    # Resolve defaults e.g. loss vs MLE vs MAP optimization
+    if loss_fn is None and likelihood is None:
+        if inference == 'frequentist':
+            loss_fn = RMSELoss()
+        else:
+            if noise is None:
+                noise = prx.Uniform(0.0, 0.01)
+            likelihood = GaussianLikelihood(noise)
+        
+    if loss_fn is not None:
+        objective_fn = TargetLoss(predictor=features, target=target, loss=loss_fn)
     else:
-        scaled_cost_fn = objective_fn
+        mll = MarginalLogLikelihood(predictor=features, observed=target, likelihood=likelihood, discrepancy=discrepancy_model)
+        if inference == 'frequentist':
+            objective_fn = NegativeLogLikelihood(mll)
+        else:
+            objective_fn = NegativeLogPosterior(mll)
 
     # Run the optimizer
-    optimize_result = minimize(scaled_cost_fn, model, frequency, solver, **kwargs)
+    optimize_result = minimize(objective_fn, model, frequency, solver, **kwargs)
 
     return FitResult(
         data=data,
